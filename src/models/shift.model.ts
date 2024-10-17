6b7eb556f5b7a35407ae96d0b2121436d3ebc197
Surajit-FR <miltonbaker.psoriatic@gmail.com>
2024-10-17 19:41:07 +0530

    Update

--- src/models/shift.model.ts
+++ src/models/shift.model.ts
@@ -1,37 +1,37 @@
-import mongoose,{Schema,Model} from "mongoose";
-import {IShiftSchema,IShiftTimeSchema} from '../../types/schemaTypes';
+import mongoose, { Schema, Model } from "mongoose";
+import { IShiftSchema, IShiftTimeSchema } from '../../types/schemaTypes';
 
 // Define a schema for shift times
-const shiftTimeSchema :Schema<IShiftTimeSchema>= new Schema({
+const shiftTimeSchema: Schema<IShiftTimeSchema> = new Schema({
   startTime: {
-    type: String,  
+    type: String,
     required: true
   },
   endTime: {
-    type: String,  
+    type: String,
     required: true
   }
 },);
 
 // Define the main Shift schema
-const shiftSchema:Schema<IShiftSchema> = new Schema({
+const shiftSchema: Schema<IShiftSchema> = new Schema({
   shiftName: {
     type: String,  // Example: 'Morning', 'Evening', 'Night'
     required: true,
     unique: true
   },
   shiftTimes: [shiftTimeSchema],  // Array of shift times for each shift
-  isDeleted:{
-    type:Boolean,
-    default:false
+  isDeleted: {
+    type: Boolean,
+    default: false
   },
   createdBy: {
-    type: mongoose.Schema.Types.ObjectId,  
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   },
-  
-},{ timestamps: true });
+
+}, { timestamps: true });
 
 // Create the Shift model
-const ShiftModel:Model<IShiftSchema> = mongoose.model<IShiftSchema>('Shift',shiftSchema);
+const ShiftModel: Model<IShiftSchema> = mongoose.model<IShiftSchema>('Shift', shiftSchema);
 export default ShiftModel;

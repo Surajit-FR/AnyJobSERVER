539b4f63a23ad073209916678a1068c187b792da
Surajit-FR <miltonbaker.psoriatic@gmail.com>
2024-10-22 18:26:55 +0530

    Changes in question route

--- src/routes/question.routes.ts
+++ src/routes/question.routes.ts
@@ -6,18 +6,17 @@ const router: Router = express.Router();
 router.use(VerifyJWTToken);
 
 router.route('/')
-.post(verifyUserType(['SuperAdmin']), addQuestions)
-.get(fetchQuestions);
+    .post(verifyUserType(['SuperAdmin']), addQuestions)
+    .get(fetchQuestions);
 
 router.route('/:categoryId').get(fetchQuestionsCategorywise);
 
-router.route('/:categoryId/:questionId').get(fetchSingleQuestion);
-
-router.route('/:categoryId/:questionId')
-    .patch(verifyUserType(["SuperAdmin"]), updateSingleQuestion);
+router.route('/q/:categoryId/:questionId')
+    .get(fetchSingleQuestion)
+    .patch(verifyUserType(["SuperAdmin"]), updateSingleQuestion)
 
 router.route('/:questionId').delete(verifyUserType(["SuperAdmin"]), deleteSingleQuestion);
-   
+
 
 
 

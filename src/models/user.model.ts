80961a3d483ac01069531fda4f08d9d201a4d792
simran3011-node <simran.agarwal@ariprodesigns.com>
2024-11-01 19:00:20 +0530

    user model modified

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,13 +1,7 @@
 import mongoose, { Schema, Model } from "mongoose";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { IUser,IAddressType,IAdditionalUserInfo } from "../../types/schemaTypes";
-import { emit } from "process";
-import { string } from "joi";
-
-
-
-
+import { IUser } from "../../types/schemaTypes";
 
 
 const UserSchema: Schema<IUser> = new Schema({

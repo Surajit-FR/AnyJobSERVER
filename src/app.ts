94f1abdbb8e7d896805d74b460079d9f8e2246ea
Surajit-FR <miltonbaker.psoriatic@gmail.com>
2025-01-06 13:10:58 +0530

    access control

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,6 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     // res.setHeader('Access-Control-Allow-Origin', 'https://frontend.theassure.co.uk');
-    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');

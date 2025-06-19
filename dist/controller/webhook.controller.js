89b29ae1ee08cc23c70193740a1e6631ba08b015
Surajit-FR <surojit.pal@ariprodesigns.com>
2025-06-19 18:59:20 +0530

    cancellation fee debug

--- dist/controller/webhook.controller.js
+++ dist/controller/webhook.controller.js
@@ -429,7 +429,7 @@ const handleTransferCreated = (transfer) => __awaiter(void 0, void 0, void 0, fu
             description = "ServiceCancellationAmount";
             const parts = transferGroup.split("_");
             console.log("parts", parts);
-            SPId = parts[2]; // Extract SPId
+            SPId = parts[3]; // Extract SPId
         }
         else if (transferGroup.startsWith("incentive_fee_")) {
             description = "ServiceIncentiveAmount";
